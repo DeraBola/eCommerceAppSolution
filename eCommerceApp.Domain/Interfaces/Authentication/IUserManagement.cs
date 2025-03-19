398d3df2fa308d880488407a0a110b06e5c01bc6
derabola <adu.deraj@gmail.com>
2025-03-19 16:17:36 -0700

    added identity package

---
+++ eCommerceApp.Domain/Interfaces/Authentication/IUserManagement.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using eCommerceApp.Domain.Entities.Identity;
+
+namespace eCommerceApp.Domain.Interfaces.Authentication
+{
+	public interface IUserManagement
+	{
+		Task<bool> CreateUser (AppUser user);
+		Task<bool> LoginUser(AppUser user);
+		Task<AppUser?> GetUserByEmail(string email);
+		Task<AppUser> GetUserById(string id);
+		Task<IEnumerable<AppUser>?> GetAllUsers();
+		Task<int> RemoverUserByEmail(string email);
+		Task<List<Claim>> GetUserClaims(string email);
+	}
+
+}
