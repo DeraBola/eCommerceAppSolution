ceeca07833f8a4bd1f49119155b15c324359c2c6
derabola <adu.deraj@gmail.com>
2025-04-02 21:32:46 -0700

    added Stripe payment service

---
+++ eCommerceApp.Host/Controllers/PaymentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceApp.Host.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PaymentController : ControllerBase
+	{
+	}
+}
